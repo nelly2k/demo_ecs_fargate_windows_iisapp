5d8cca8264de95f89e693a80fa74a52f788d1689
Nelli Lovchikova <nelly2k@gmail.com>
2022-01-07 15:51:17 +1100

    Initial commit

---
+++ src/DogsAreTheBest/DogsAreTheBestStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace DogsAreTheBest
+{
+    public class DogsAreTheBestStack : Stack
+    {
+        internal DogsAreTheBestStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
